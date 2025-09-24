510ca2b08c834a68d80fdcc609adfe77a43d292b
Talor Gruenwald <talor.gruenwald@gmail.com>
2025-09-24 01:15:56 -0400

    adjusting batch jobs

--- submit_one.sh
+++ submit_one.sh
@@ -6,11 +6,11 @@ JOB_TS=$(date -u +"%Y%m%d-%H%M%S")
 
 
 # Define job name with timestamp
-JOB_NAME="dgen-virginia-${JOB_TS}"
+JOB_NAME="dgen-mass-${JOB_TS}"
 
 # submit the third job
 gcloud batch jobs submit "${JOB_NAME}" \
   --location="${LOCATION}" \
-  --config="batch_job_yamls/dgen-batch-job-mid-large-states.yaml" \
-  --machine-type="c2d-highcpu-32"  \
+  --config="batch_job_yamls/dgen-batch-job-mid-states.yaml" \
+  --machine-type="c2d-highcpu-16"  \
   --provisioning-model="SPOT"
