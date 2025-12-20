3d801bf60c5aaa0ed2e53a1ad14df7bf35d042f0
Talor Gruenwald <talor.gruenwald@gmail.com>
2025-12-19 22:04:52 -0500

    Adjusting prices to create a maryland scenario

--- submit_one.sh
+++ submit_one.sh
@@ -6,11 +6,11 @@ JOB_TS=$(date -u +"%Y%m%d-%H%M%S")
 
 
 # Define job name with timestamp
-JOB_NAME="dgen-ny-${JOB_TS}"
+JOB_NAME="dgen-md-${JOB_TS}"
 
 # submit the third job
 gcloud batch jobs submit "${JOB_NAME}" \
   --location="${LOCATION}" \
-  --config="batch_job_yamls/dgen-batch-job-mid-large-states.yaml" \
-  --machine-type="c2d-highcpu-32"  \
+  --config="batch_job_yamls/dgen-batch-job-mid-states.yaml" \
+  --machine-type="c2d-highcpu-16"  \
   --provisioning-model="SPOT"
